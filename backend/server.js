4bf7748ac3a5a1b99f13846dcbb0f59982b51645
Hahinika Dilrukshi <ghashinika2002@gmail.com>
2024-05-08 09:12:17 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3001;
+const host = 'localhost';
+const mongoose = require('mongoose');
+const router = require('./router');
+
+app.use(cors());
+app.use(express.json());
+
+const uri = 'mongodb+srv://dhashinika:hashinika123@cluster0.oqvwhda.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+const connect = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to MongoDB');
+    } catch (error) { // Define the error parameter here
+        console.log('MongoDB Error:', error);
+    }
+};
+connect();
+
+const server = app.listen(port, host, () => {
+    console.log(`Node server is listening to ${server.address().port}`);
+});
+
+app.use('/api' , router);
